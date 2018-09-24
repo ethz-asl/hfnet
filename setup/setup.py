755e518aa1164951f7be32e2379a307246530b84
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2018-09-25 00:11:25 +0200

    Fix installation

--- setup/setup.py
+++ setup/setup.py
@@ -1,7 +1,8 @@
+import os
+from pathlib import Path
+os.chdir(Path(Path(__file__).parent, '..').as_posix())
 from setuptools import setup
-
 
 setup(name='hfnet',
       version="0.0",
-      packages=['hfnet'],
-      package_dir={'hfnet': '../hfnet/'})
+      packages=['hfnet'])
