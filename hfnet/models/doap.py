a719f3461fa26f95b640d3159901aa285e0320f6
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2018-10-20 13:26:13 +0200

    Add DOAP with spatial transformer
    Former-commit-id: c07eaf676794c2f71e05f8ab0dd62297ec62ce0f

--- hfnet/models/doap.py
+++ hfnet/models/doap.py
@@ -23,43 +23,63 @@ class Doap(BaseModel):
     required_config_keys = []
     default_config = {
             'use_transformer': False,
-            'descriptor_layer': 'logits',
+            'descriptor_layer': None,
             'weights': '',
     }
 
     def _model(self, inputs, mode, **config):
         assert config['weights']
+        is_simplenn = 'ST' not in Path(config['weights']).stem
         path = Path(DATA_PATH, 'weights', config['weights']).as_posix()
         mat = scio.loadmat(path, struct_as_record=False, squeeze_me=True)
         net = mat['net']
-        params = {p.name: p.value for p in net.params}
         layers = net.layers
+
+        if not is_simplenn:
+            params = {p.name: p.value for p in net.params}
 
         tf_layers = {}
         image = normalize(inputs['image'])
-        if config['use_transformer']:
-            tf_layers['input'] = image
+        if is_simplenn or config['use_transformer']:
+            previous = 'input'
             start = 0
         else:
-            tf_layers['xST'] = image
+            previous = 'xST'
             start = [l.name for l in layers].index('conv1')
+        tf_layers[previous] = image
         end = len(layers) - 2  # skip loss
 
         for i in range(start, end+1):
             layer = layers[i]
-            logging.info(f'Reading layer {layer.name}')
+            if is_simplenn:
+                layer.inputs = previous
+                previous = layer.outputs = layer.name
+            logging.info(f'Reading layer {layer.name:^15} '
+                         f'{layer.inputs:} --> {layer.outputs}')
 
-            if layer.type == 'dagnn.Conv':
-                shape = layer.block.size
-                k1, k2, c_in, c_out = shape
-                w, b = layer.params  # weight, bias
-                w, b = params[w], params[b]
-                w = w.reshape(shape)
+            if layer.type in ['dagnn.Conv', 'conv']:
+                if is_simplenn:
+                    w, b = layer.weights  # weight, bias
+                    if len(w.shape) == 3:  # Matlab skips channels if 1
+                        w = w[:, :, np.newaxis]
+                    k1, k2, c_in, c_out = w.shape
+                else:
+                    shape = layer.block.size
+                    k1, k2, c_in, c_out = shape
+                    w, b = layer.params  # weight, bias
+                    w, b = params[w], params[b]
+                    w = w.reshape(shape)
                 assert c_out == b.shape[0]
 
-                stride = layer.block.stride
-                pad = layer.block.pad.reshape((2, 2))
-                assert np.all(layer.block.dilate == 1)
+                if is_simplenn:
+                    stride = np.array([layer.stride]*2)
+                    pad = np.array([layer.pad]*4).reshape((2, 2))
+                    dilate = np.array([layer.dilate]*2)
+                else:
+                    stride = layer.block.stride
+                    pad = layer.block.pad.reshape((2, 2))
+                    dilate = layer.block.dilate
+                assert np.all(dilate == 1)
                 conv_pad = 'valid'
 
                 if layer.name == 'conv7':  # preserve shape in last conv
@@ -78,20 +98,23 @@ class Doap(BaseModel):
                     kernel_initializer=w, bias_initializer=b)
                 tf_layers[layer.outputs] = tf_layer
 
-                logging.info(
-                    f'Output {str(tf_layer.shape):>10} {str(pad):>10}')
+                logging.info(f'Output {str(tf_layer.shape):>10} {pad.max()}')
 
-            elif layer.type == 'dagnn.ReLU':
-                assert layer.block.leak == 0
+            elif layer.type in ['dagnn.ReLU', 'relu']:
+                assert (layer.leak if is_simplenn else layer.block.leak) == 0
                 input_tensor = tf_layers[layer.inputs]
                 tf_layer = tf.nn.relu(input_tensor)
                 tf_layers[layer.outputs] = tf_layer
 
-            elif layer.type == 'dagnn.BatchNorm':
-                w, b, m = layer.params  # weight, bias, moments
-                w, b, m = params[w], params[b], params[m]
+            elif layer.type in ['dagnn.BatchNorm', 'bnorm']:
+                if is_simplenn:
+                    w, b, m = layer.weights  # weight, bias, moments
+                    epsilon = layer.epsilon
+                else:
+                    w, b, m = layer.params  # weight, bias, moments
+                    w, b, m = params[w], params[b], params[m]
+                    epsilon = layer.block.epsilon
                 mean, std = m[:, 0], m[:, 1]
-                epsilon = layer.block.epsilon
 
                 input_tensor = tf_layers[layer.inputs]
                 w = tf.constant_initializer(w)
@@ -106,7 +129,7 @@ class Doap(BaseModel):
                     training=False)
                 tf_layers[layer.outputs] = tf_layer
 
-            elif layer.type == 'dagnn.DropOut':
+            elif layer.type in ['dagnn.DropOut', 'dropout']:
                 input_tensor = tf_layers[layer.inputs]
                 tf_layers[layer.outputs] = input_tensor
 
@@ -117,7 +140,10 @@ class Doap(BaseModel):
                 raise NotImplementedError
 
             else:
-                raise NotImplementedError
+                raise NotImplementedError(layer.type)
+
+        if config['descriptor_layer'] is None:
+            config['descriptor_layer'] = previous if is_simplenn else 'logits'
 
         desc = tf_layers[config['descriptor_layer']]
         desc = tf.nn.l2_normalize(desc, axis=-1)
