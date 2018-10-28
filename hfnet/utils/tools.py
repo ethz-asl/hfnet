5497a42430a0ca72a6fd8e71aa4c35c341f3c1db
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2018-10-28 09:07:37 +0100

    Add profiling tool
    Former-commit-id: 0fb5ef1afeccab18544e1182503d0c262c71693a

--- hfnet/utils/tools.py
+++ hfnet/utils/tools.py
@@ -1,4 +1,5 @@
 import collections
+import time
 
 
 def dict_update(d, u):
@@ -18,3 +19,15 @@ def dict_update(d, u):
         else:
             d[k] = v
     return d
+
+
+class Timer(object):
+    def __init__(self, name=None):
+        self.name = name
+
+    def __enter__(self):
+        self.tstart = time.time()
+
+    def __exit__(self, type, value, traceback):
+        prefix = ('[%s]' % self.name) if self.name else ''
+        print('%s Elapsed: %s' % (prefix, time.time() - self.tstart))
