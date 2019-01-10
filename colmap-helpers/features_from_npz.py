Fix bug with keypoint scaling, cleaner export for SfM
Former-commit-id: a9419f0ac47ba706e1c914097f8f499a6403278d

colmap-helpers/features_from_npz.py
@@ -1,5 +1,4 @@
 import argparse
-import cv2
 import numpy as np
 import os
 from tqdm import tqdm
@@ -21,24 +20,15 @@ def export_features_from_npz(filename, in_path, out_path):
 
     filename = os.path.splitext(os.path.basename(path_file))[0]
 
-    # Load a corresponding image.
-    img_file = os.path.join(out_path, filename) + '.jpg'
-    img = cv2.imread(img_file, 0)
-    original_height, original_width = img.shape
-
     out_path_and_name = os.path.join(out_path, filename) + '.jpg.txt'
     outfile = open(out_path_and_name, "w+")
 
-    scaling = np.array(img.shape)[::-1] / np.array(frame1['image_size']).astype(np.float)
-
     SIFT_SIZE = 128
-    # Scale the keypoints according to the scaling factor.
-    kp1 = frame1['keypoints'] * scaling
+    kp1 = frame1['keypoints']
     outfile.write(str(kp1.shape[0]) + ' ' + str(SIFT_SIZE) + '\n')
 
     for keypoint in kp1:
         outfile.write(str(keypoint[0]) + ' ' + str(keypoint[1]) + ' 1 1 ')
-        assert (keypoint[0] <= original_width)
         # Generate some dummy SIFT values as we will anyway use external
         # from a matches.txt file.
         for x in range(0, SIFT_SIZE):
@@ -53,7 +43,7 @@ def export_feature_detections():
     total_files = len(os.listdir(args.npz_dir))
     for filename in tqdm(os.listdir(args.npz_dir), total=total_files, unit='npz'):
         if filename.endswith(".npz"):
-             export_features_from_npz(filename, args.npz_dir, args.image_dir)
+            export_features_from_npz(filename, args.npz_dir, args.image_dir)
 
 
 if __name__ == "__main__":

hfnet/export_for_sfm.py
@@ -13,9 +13,8 @@ from hfnet.settings import EXPER_PATH  # noqa: E402
 
 
 def export_for_sfm(data_config, exper_config, export_name):
-    # export_name = exper_config['experiment']+'_sfm'
     export_dir = Path(EXPER_PATH, 'exports', export_name)
-    export_dir.mkdir()
+    export_dir.mkdir(exist_ok=True)
 
     dataset = get_dataset(data_config['name'])(**data_config)
     data_iter = dataset.get_test_set()
@@ -23,8 +22,12 @@ def export_for_sfm(data_config, exper_config, export_name):
     for data in tqdm(data_iter):
         predictions = exper_config['predictor'](
             data['image'], data['name'], **exper_config)
+        # Scale the keypoints to the original image size
+        # and convert to Colmap convention (origin = corner of upper left pix)
+        scale = ((np.array(data['original_size'][:2]) - 1)
+                 / (np.array(data['image'].shape[:2]) - 1))
         export = {
-            'keypoints': predictions['keypoints'],
+            'keypoints': scale[::-1] * predictions['keypoints'] + 0.5,
             'scores': predictions['scores'],
             'descriptors': predictions['descriptors'],
             'image_size': data['image'].shape[:2][::-1]
@@ -36,15 +39,31 @@ def export_for_sfm(data_config, exper_config, export_name):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('dataset', type=str)
     parser.add_argument('export_name', type=str)
     parser.add_argument('new_export_name', type=str)
     args = parser.parse_args()
 
-    data_config = {
-        'name': 'aachen',
-        'load_db': True,
-        'load_queries': False,
-        'resize_max': 960,
+    data_configs = {
+        'aachen': {
+            'name': 'aachen',
+            'load_db': True,
+            'load_queries': False,
+            'resize_max': 960,
+        },
+        'robotcar': {
+            'name': 'robotcar',
+            'sequences': ['overcast-reference'],
+            'resize_max': 960,
+        },
+        'cmu': {
+            'name': 'cmu',
+            'sequences': ['slice2', 'slice3', 'slice4', 'slice5', 'slice6',
+                          'slice7', 'slice8', 'slice9', 'slice10', 'slice17'],
+            'load_db': True,
+            'load_queries': False,
+            'resize_max': 1024,
+        }
     }
 
     exper_config = {
@@ -55,4 +74,5 @@ if __name__ == '__main__':
         'nms_thresh': 4,
     }
 
-    export_for_sfm(data_config, exper_config, args.new_export_name)
+    export_for_sfm(
+        data_configs[args.dataset], exper_config, args.new_export_name)